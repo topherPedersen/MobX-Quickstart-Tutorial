ee7322371fc32c07a422cb217a3758bf57469407
Christopher Pedersen <chris@topherpedersen.com>
2019-11-21 15:48:56 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { observable, computed, reaction, action } from 'mobx';
+
+class TodoStore {
+  todos = [];
+
+  get completedTodosCount() {
+    return this.todos.filter(
+      todo => todo.completed === true
+    ).length;
+  }
+
+  report() {
+    if (this.todos.length === 0) 
+      return "<none>";
+    return `Next todo: "${this.todos[0].task}". ` + `Progress: ${this.completedTodosCount}/${this.todos.length}`;
+  }
+
+  addTodo(task) {
+    this.todos.push({
+      task: task,
+      completed: false,
+      assignee: null
+    });
+  }
+}
+
+const todoStore = new TodoStore();
+
+todoStore.addTodo("read MobX tutorial");
+console.log(todoStore.report());
+
+todoStore.addTodo("try MobX");
+console.log(todoStore.report());
+
+todoStore.todos[0].completed = true;
+console.log(todoStore.report());
+
+todoStore.todos[1].task = "try MobX in own project";
+console.log(todoStore.report());
+
+todoStore.todos[0].task = "grok MobX tutorial";
+console.log(todoStore.report());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, MobX</h1>
     </div>
   );
 }
